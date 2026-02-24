bde27b0ce510b7686ef1597d01c017263369892a
Yashaswini Mudragadda <mudragaddarajaniyashaswini@gmail.com>
2026-02-24 11:58:58 +0530

    Create test03.js

---
+++ test03.js
@@ -0,0 +1,20 @@
+//code for print employee data
+
+ecode="E101"
+eno=101
+ename="anil"
+esal=455.85
+egrade='a'
+ews=true
+console.log("ecode= "+ecode+"    "+typeof(ecode))
+console.log("eno= "+eno+"    "+typeof(eno))
+console.log("ename= "+ename+"    "+typeof(ename))
+console.log("esal= "+esal+"    "+typeof(esal))
+console.log("egrade= "+egrade+"    "+typeof(egrade))
+console.log("ews= "+ews+"    "+typeof(ews))
+
+/**bno= 1 number
+bcode= b101 string
+bav= true boolean
+bsales= null object
+bgst= undefined undefined**/
